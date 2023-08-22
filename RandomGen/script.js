8964329bdefe24d35102da5de16847132756ee24
Amil Ali <amil13985@gmail.com>
2023-08-22 22:57:15 +0530

    add

--- RandomGen/script.js
+++ RandomGen/script.js
@@ -1,15 +1,15 @@
 let h2 = document.querySelector("h2");
 
 let button = document.querySelector("button");
-
+// h2.textContent = "Please Wait";
 NumGenerator = ()=>{
     let num = Math.random().toString(8).slice(2,3);
-    if(num > 1 && num<7)
+    if(num > 1 && num<=6)
     {
     h2.textContent = num;
     }
     else{
-        h2.textContent = num;
+        h2.textContent = 1;
     }
 }
 button.addEventListener("click", NumGenerator);
