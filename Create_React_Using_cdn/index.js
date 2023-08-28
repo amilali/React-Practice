79e3dba078ae8dfc631ba6a1ff5801ea80d56e76
Amil Ali <amil13985@gmail.com>
2023-08-28 17:41:26 +0530

    add

--- Create_React_Using_cdn/index.js
+++ Create_React_Using_cdn/index.js
@@ -1,5 +1,7 @@
 const MyApp = () => <div> <b>Hello Amil!</b> </div>;
 
-const rootContainer = document.getElementById("root");
-const root = ReactDOM.createRoot(rootContainer);
-root.render(<MyApp />);
+// const rootContainer = document.getElementById("root");
+// const root = ReactDOM.createRoot(rootContainer);
+// root.render(<MyApp />);
+
+ReactDOM.render(<MyApp />, document.getElementById("root"));
