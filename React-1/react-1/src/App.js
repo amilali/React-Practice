8d46ccb624cc321954ad5cbf5bfa4579f67d86b2
Amil Ali <amil13985@gmail.com>
2023-08-29 17:23:12 +0530

    add

--- React-1/react-1/src/App.js
+++ React-1/react-1/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Mydp from './Mydp';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Mydp />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
          Welcome To Amil's PC
---
+++ React-1/react-1/src/Mydp.js
@@ -0,0 +1,7 @@
+function Mydp(){
+    return(
+        <img src="https://raw.githubusercontent.com/amilali/CrowdXfund/main/img/circular%20dp.png" alt="dp" height="100px" opacity="1"/>
+    );
+}
+
+export default Mydp;
