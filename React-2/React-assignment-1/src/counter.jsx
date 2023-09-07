8064081ee9eb3abbde07eec1b8176c3a5d79ea72
Amil Ali <amil13985@gmail.com>
2023-09-07 15:46:00 +0530

    add

--- React-2/React-assignment-1/src/counter.jsx
+++ React-2/React-assignment-1/src/counter.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react';
 const Counter = () =>{
 
-    const [ x, setx ] = useState(0);
+    let [ x, setx ] = useState(0);
     return(
         <>
-        <h1>{x}</h1>
+        <h1>{x} is {(x%2==0) ? 'Even' : 'odd'}  </h1>
         <button onClick={()=>setx(x+1)}>+1</button>
         <button onClick={()=>setx(x-1)}>-1</button>
+        <button onClick={()=>setx(x=0)}>Reset</button>
         </>
     );
 }
